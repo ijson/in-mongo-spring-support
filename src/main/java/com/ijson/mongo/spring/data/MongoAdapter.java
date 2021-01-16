db64ec3339d1fc0effcba07673e455d72d17499f
cuiyongxu <cuiyongxu@gmail.com>
2021-01-16 15:11:21 +0800

    rm auth

--- src/main/java/com/ijson/mongo/spring/data/MongoAdapter.java
+++ src/main/java/com/ijson/mongo/spring/data/MongoAdapter.java
@@ -1,14 +1,14 @@
-package com.ijson.mongo.spring.data;
-
-import com.mongodb.client.MongoClient;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-public class MongoAdapter extends MongoTemplate {
-
-    private String configName;
-
-    public MongoAdapter(MongoClient mongoClient, String databaseName) {
-        super(mongoClient, databaseName);
-    }
-}
+//package com.ijson.mongo.spring.data;
+//
+//import com.mongodb.client.MongoClient;
+//import org.springframework.data.mongodb.MongoDatabaseFactory;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//
+//public class MongoAdapter extends MongoTemplate {
+//
+//    private String configName;
+//
+//    public MongoAdapter(MongoClient mongoClient, String databaseName) {
+//        super(mongoClient, databaseName);
+//    }
+//}
--- src/main/java/com/ijson/mongo/spring/data/impl/IDaoImpl.java
+++ src/main/java/com/ijson/mongo/spring/data/impl/IDaoImpl.java
@@ -1,58 +1,58 @@
-package com.ijson.mongo.spring.data.impl;
-
-import com.ijson.mongo.spring.data.IDao;
-import com.ijson.mongo.support.model.BaseEntity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-import java.util.List;
-
-public abstract class IDaoImpl<T extends BaseEntity> implements IDao<T> {
-
-    //@Autowired
-    public MongoTemplate mongoTemplate;
-
-    /**
-     * 数据插入  主键存在 报错:org.springframework.dao.DuplicateKeyException
-     *
-     * @param entity
-     * @param <T>
-     * @return
-     */
-    @Override
-    public <T> T insert(T entity) {
-        return mongoTemplate.insert(entity);
-    }
-
-    /**
-     * 数据更新
-     *
-     * @param entity
-     * @param <T>
-     * @return
-     */
-    @Override
-    public <T> T update(T entity) {
-        return mongoTemplate.save(entity);
-    }
-
-    /**
-     * 批量插入
-     * @param entity
-     */
-    @Override
-    public void batchInsert(List<T> entity) {
-        mongoTemplate.insert(entity);
-    }
-
-    @Override
-    public void delete(String... ids) {
-        mongoTemplate.remove(ids);
-
-    }
-
-    @Override
-    public T find(String id) {
-        return null;
-    }
-}
+//package com.ijson.mongo.spring.data.impl;
+//
+//import com.ijson.mongo.spring.data.IDao;
+//import com.ijson.mongo.support.model.BaseEntity;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//
+//import java.util.List;
+//
+//public abstract class IDaoImpl<T extends BaseEntity> implements IDao<T> {
+//
+//    //@Autowired
+//    public MongoTemplate mongoTemplate;
+//
+//    /**
+//     * 数据插入  主键存在 报错:org.springframework.dao.DuplicateKeyException
+//     *
+//     * @param entity
+//     * @param <T>
+//     * @return
+//     */
+//    @Override
+//    public <T> T insert(T entity) {
+//        return mongoTemplate.insert(entity);
+//    }
+//
+//    /**
+//     * 数据更新
+//     *
+//     * @param entity
+//     * @param <T>
+//     * @return
+//     */
+//    @Override
+//    public <T> T update(T entity) {
+//        return mongoTemplate.save(entity);
+//    }
+//
+//    /**
+//     * 批量插入
+//     * @param entity
+//     */
+//    @Override
+//    public void batchInsert(List<T> entity) {
+//        mongoTemplate.insert(entity);
+//    }
+//
+//    @Override
+//    public void delete(String... ids) {
+//        mongoTemplate.remove(ids);
+//
+//    }
+//
+//    @Override
+//    public T find(String id) {
+//        return null;
+//    }
+//}
