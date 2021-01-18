1476f39be7011277385051386c9568ba39395112
cuiyongxu <cuiyongxu@gmail.com>
2021-01-18 20:11:51 +0800

    add doc

--- src/test/java/com/ijson/mongo/support/test/bean/Question.java
+++ src/test/java/com/ijson/mongo/support/test/bean/Question.java
@@ -12,13 +12,21 @@ public class Question extends BaseEntity {
     private String id;
     @DocDescribe("标题")
     private String title;
+    @DocDescribe("内容")
     private String content;
+    @DocDescribe("阅读")
     private Long view;
+    @DocDescribe("点赞")
     private Long praise;
+    @DocDescribe("收藏")
     private Long collect;
+    @DocDescribe("回答总数")
     private Long answer;
+    @DocDescribe("分类id")
     private String categoryId;
+    @DocDescribe("奖励")
     private Long reward;
+    @DocDescribe("标签")
     private List<String> tagIds;
 
 }
